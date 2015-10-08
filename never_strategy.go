ddcfdd9fe75f634600923fc5e1a422bf647f424a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2015-10-08 16:12:40 -0700

    tweaked docs

--- never_strategy.go
+++ never_strategy.go
@@ -4,6 +4,7 @@ package retoil
 type internalNeverStrategy struct{}
 
 
+// NeverStrategy returns an initialized retoil.Strategizer which will never allow a retoil.
 func NeverStrategy() Strategizer {
 	strategy := internalNeverStrategy{}
 
